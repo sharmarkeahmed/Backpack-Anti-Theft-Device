dc8d957bb748a9578b741038caf66d79884508ab
Ryanscapist <152208637+Ryanscapist@users.noreply.github.com>
2023-12-09 13:52:44 -0600

    Update LightSensor.h

--- final_project.X/LightSensor.h
+++ final_project.X/LightSensor.h
@@ -13,6 +13,9 @@ extern "C" {
 #endif
 
 void initLightSensor();
+/**
+ * checks if light detected is above the voltage threshold needed to set off alarm (2.5 V)
+ */
 int lightDetected();
 
 #ifdef	__cplusplus
