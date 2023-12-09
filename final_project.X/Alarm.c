887a08ee4e3223f513ce092128b790f50a5c105a
Ryanscapist <152208637+Ryanscapist@users.noreply.github.com>
2023-12-09 15:37:28 -0600

    Update Alarm.c

--- final_project.X/Alarm.c
+++ final_project.X/Alarm.c
@@ -5,8 +5,8 @@
  * continuous sound when connected to 3.3V on pin RP14 on the PIC24FJ64GA002.
  * The library uses Timer2 and the output compare 1 register on the 
  * microcontroller. Ensure these modules are not being used elsewhere. When the
- * alarm is turned on, the library will send pulses to the buzzer with a
- * frequency of 0.476 Hz.
+ * alarm is turned on, the library will send pulses to the buzzer with an
+ * input frequency greater than 0.477 Hz.
  *
  * Created on November 23, 2023, 4:11 PM
  */
@@ -26,8 +26,8 @@ void turnOffAlarm();
  * the output compare register and Timer2 used to send pulses to the buzzer.
  * 
  * @param freq the minimum frequency allowed is 0.4768444343 Hz, anything lower
- * than this will overflow the period.
- * The frequency at which that alarm beeps can be altered by the user
+ * than this will overflow the period. The frequency at which that alarm beeps 
+ * can be altered by the user
  */
 void initAlarm(double freq) {
     CLKDIVbits.RCDIV = 0; // 16 MHz clock
