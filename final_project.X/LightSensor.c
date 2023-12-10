58df0b2fb5aba399da734384dedff9364afa0f18
Sharmarke Ahmed <101375936+sharmarkeahmed@users.noreply.github.com>
2023-12-10 14:08:59 -0600

    Update LightSensor.c
    Added THRESHOLD preprocesor directive

--- final_project.X/LightSensor.c
+++ final_project.X/LightSensor.c
@@ -15,10 +15,7 @@
 
 #define BUFSIZE 10
 #define NUMSAMPLES 128
-#define BATTERYVOLT 3
-#define COMPVOLT 3.3
-#define COMPTHRESHOLD 2.5
-#define BATTERYTHRESHOLD 2.5
+#define THRESHOLD 2
 volatile int adc_buffer[BUFSIZE];
 volatile int buffer_index = 0;
 volatile float adcval = 0;
@@ -85,7 +82,6 @@ void initBuffer(){ //initializes buffer to size of 10, no arguments, no return v
 /**
  * Takes the value from the buffer and puts it in the array.
  */
-
 void putVal(int ADCvalue){ //Fills buffer with ADC values, the ADC value is the argument, no return values
     adc_buffer[buffer_index++] = ADCvalue;
     if(buffer_index >= BUFSIZE){
@@ -96,7 +92,6 @@ void putVal(int ADCvalue){ //Fills buffer with ADC values, the ADC value is the 
 /**
  * averages the values of the array.
  */
-
 int getAvg(){ // averages the values in the buffer, no arguments, no return values
     unsigned long int sum = 0;
     int average;
@@ -119,13 +114,12 @@ void __attribute__((__interrupt__, __auto_psv__)) _ADC1Interrupt(){ //everytime 
 /**
  * checks if light detected is above the voltage threshold needed to set off alarm (2.5 V)
  */
-
 int lightDetected(){
     adcval = ((float) 3.0 /1024)*getAvg(); //dark = 3.29, partially open = 1.743 w/ 3.3 V source || dark = 2.997, partially open = 1.863 w/ 3.0 V source
     if(adc_buffer[9] == 0){ //buffer is not full in progress
         return -1;
     }
-    else if(adcval <= 2.5){ //open backpack
+    else if(adcval <= THRESHOLD){ //open backpack
         return 1;
     }
         return 0;
